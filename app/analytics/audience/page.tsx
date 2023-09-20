6d6c0c20836596406e3bc5d1924975cc0db85f00
Joseph D. Clark <josephclark@Josephs-MacBook-Air.local>
2023-09-20 08:11:40 -0500

    analytics part 2 branch

--- app/analytics/audience/page.tsx
+++ app/analytics/audience/page.tsx
@@ -3,10 +3,10 @@ import React, { lazy, Suspense } from "react";
 export default function Audience() {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-8">
-      <h1 className="text-xl font-medium mb-8">Audience Demographics</h1>
-
+      <h1 className="text-xl font-medium ">Audience Demographics</h1>
+      <div className="w-3/4 h-3/4 bg-gray-300 rounded shadow-lg">Graph</div>
       <div className="flex space-x-8">
-        <div className="flex flex-col space-y-4">
+        <div className="flex space-x-4">
           <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center">
             Age
           </button>
@@ -14,8 +14,7 @@ export default function Audience() {
             Location
           </button>
         </div>
-
-        <div className="flex flex-col space-y-4">
+        <div className="flex space-x-4">
           <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center">
             Gender
           </button>
