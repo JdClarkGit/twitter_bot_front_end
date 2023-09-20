ee659182e9e26f174f9fbeba9c95adf9498bf229
Joseph D. Clark <josephclark@Josephs-MacBook-Air.local>
2023-09-20 06:56:21 -0500

    Added functionality to the analytics page

--- app/analytics/audience/page.tsx
+++ app/analytics/audience/page.tsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 
 export default function Audience() {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-8">
-      <h1 className="text-xl font-medium mb-8">Audience</h1>
+      <h1 className="text-xl font-medium mb-8">Audience Demographics</h1>
 
       <div className="flex space-x-8">
         <div className="flex flex-col space-y-4">
           <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center">
-            
+            Age
           </button>
           <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center">
-
+            Location
           </button>
         </div>
 
         <div className="flex flex-col space-y-4">
-          <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center"></button>
-          <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center"></button>
+          <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center">
+            Gender
+          </button>
+          <button className="cursor-pointer w-[200px] h-16 bg-blue-500 text-white rounded-lg shadow-md transition-all duration-200 transform active:scale-90 flex items-center justify-center">
+            Active Times
+          </button>
         </div>
       </div>
     </div>
--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,47 +1,125 @@
 "use client";
-import React from "react";
-import Link from "next/link";
+import React, { useState } from "react";
+import Audience from "./audience/page";
+import ConversionRate from "./conversion/page";
+import CustomDataRange from "./customdata/page";
+import Engagement from "./engagement/page";
+import ExportData from "./exportdata/page";
+import HistoryComparison from "./history/page";
+import Impressions from "./impressions/page";
+import IntegrationStats from "./integration/page";
+import DashboardOverview from "./overview/page";
+import PlatformBreakdown from "./platformbreak/page";
+import PostAnalytics from "./postanalytics/page";
+import Recommendations from "./recommendations/page";
+import Sentiment from "./sentiment/page";
 
 export default function DashboardPage() {
+  const [activePage, setActivePage] = useState(null);
+
   const menuItems = [
     {
       alt: "Dashboard Overview",
       src: "/path/to/notes-logo.png",
-      onClick: null,
+      onClick: () => setActivePage("DashboardOverview"),
     },
-    { alt: "Engagement", src: "/path/to/image.png", onClick: null },
-    { alt: "Impressions", src: "/path/to/image.png", onClick: null },
-    { alt: "Audience", src: "/path/to/notes-logo.png", href: "/audience" },
-    { alt: "Post Analytics", src: "/path/to/notes-logo.png", onClick: null },
-    { alt: "Platform Breakdown", src: "/path/to/image.png", onClick: null },
-    { alt: "Sentiment Analysis", src: "/path/to/image.png", onClick: null },
-    { alt: "Conversion %", src: "/path/to/image.png", onClick: null },
-    { alt: "Recommendations", src: "/path/to/image.png", onClick: null },
-    { alt: "Custom Data", src: "/path/to/image.png", onClick: null },
-    { alt: "Export Data", src: "/path/to/image.png", onClick: null },
-    { alt: "Integration Stats", src: "/path/to/image.png", onClick: null },
-    { alt: "History", src: "/path/to/image.png", onClick: null },
+    {
+      alt: "Engagement",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("Engagement"),
+    },
+    {
+      alt: "Impressions",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("Impressions"),
+    },
+    {
+      alt: "Audience",
+      src: "/path/to/notes-logo.png",
+      onClick: () => setActivePage("Audience"),
+    },
+    {
+      alt: "Post Analytics",
+      src: "/path/to/notes-logo.png",
+      onClick: () => setActivePage("PostAnalytics"),
+    },
+    {
+      alt: "Platform Breakdown",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("PlatformBreakdown"),
+    },
+    {
+      alt: "Sentiment Analysis",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("Sentiment"),
+    },
+    {
+      alt: "Conversion %",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("ConversionRate"),
+    },
+    {
+      alt: "Recommendations",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("Recommendations"),
+    },
+    {
+      alt: "Custom Data",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("CustomDataRange"),
+    },
+    {
+      alt: "Export Data",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("ExportData"),
+    },
+    {
+      alt: "Integration Stats",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("IntegrationStats"),
+    },
+    {
+      alt: "History",
+      src: "/path/to/image.png",
+      onClick: () => setActivePage("HistoryComparison"),
+    },
   ];
   const commonClasses =
-    "w-auto px-4 mt-2 h-12 bg-black-600 hover:bg-green-500 rounded-lg shadow-lg transition-all duration-200 transform hover:scale-95 flex items-center justify-center border border-green-600";
+    "w-auto px-4 mt-2 h-12 bg-gray-600 hover:bg-blue-500 rounded-lg shadow-lg transition-all duration-200 transform hover:scale-95 flex items-center justify-center border border-blue-600";
 
   return (
     <div className="flex h-screen w-screen bg-gray-900 text-white flex-col">
-      <nav className="flex flex-wrap justify-center p-4 bg-black border border-green-600 rounded-lg shadow-md overflow-x-auto">
+      <nav className="flex flex-wrap justify-center p-4 bg-gray border border-blue-600 rounded-lg shadow-md overflow-x-auto">
         {menuItems.map((item, index) => (
           <div key={index} className="mx-2">
             {item.href ? (
-              <Link href={item.href} className={commonClasses}>
-                {item.alt}
-              </Link>
+              <a href={item.href}>
+                <a className={commonClasses}>{item.alt}</a>
+              </a>
             ) : (
-              <button onClick={() => item.onClick} className={commonClasses}>
+              <button
+                onClick={() => item.onClick && item.onClick()}
+                className={commonClasses}
+              >
                 {item.alt}
               </button>
             )}
           </div>
         ))}
       </nav>
+      {activePage === "DashboardOverview" && <DashboardOverview />}
+      {activePage === "Engagement" && <Engagement />}
+      {activePage === "Audience" && <Audience />}
+      {activePage === "Sentiment" && <Sentiment />}
+      {activePage === "ConversionRate" && <ConversionRate />}
+      {activePage === "CustomDataRange" && <CustomDataRange />}
+      {activePage === "ExportData" && <ExportData />}
+      {activePage === "HistoryComparison" && <HistoryComparison />}
+      {activePage === "Impressions" && <Impressions />}
+      {activePage === "IntegrationStats" && <IntegrationStats />}
+      {activePage === "PlatformBreakdown" && <PlatformBreakdown />}
+      {activePage === "PostAnalytics" && <PostAnalytics />}
+      {activePage === "Recommendations" && <Recommendations />}
     </div>
   );
 }
