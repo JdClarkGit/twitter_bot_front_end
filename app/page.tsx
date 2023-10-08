3a089562b4c69006e1656606173c15cc9e2f7de8
zwickers <jaz2130@columbia.edu>
2023-10-08 17:30:48 -0600

    landing page skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 import BaseDemoVideo from "@/components/BaseDemoVideo";
 import TweetBotHeader from "@/components/TweetBotHeader";
 import LogInBtn from "@/components/LogInBtn";
+import {
+  Badge,
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Container,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Text,
+  Spacer,
+  Divider,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -16,23 +31,175 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Middle Section - BaseDemoVideo */}
-      <div className="flex-grow flex items-center justify-center">
-        <div className="w-1/2">
-          <BaseDemoVideo />
-          {/* <DemoButton /> */}
-        </div>
-      </div>
+      <Container
+        minH={"300px"}
+        maxW={"container.xl"}
+        paddingBottom={"20px"}
+        borderTop="1px solid black"
+        borderBottom="1px solid black"
+      >
+        <Text
+          fontSize="3xl"
+          color="black"
+          lineHeight="tall"
+          textAlign="center"
+          as="b"
+        >
+          TODO: Top level CTA
+        </Text>
+      </Container>
 
-      {/* Footer Section */}
-      <footer className="w-full bg-blue-500 font-bold text-white tracking-tight text-center p-4">
-        <p className="text-3xl tracking-tight">
-          In the vast tapestry of the social media universe, patterns emerge...
-          We've decoded that rhythm. Enter SocialSkyrocket: your autopilot for
-          tweets, threads, and brand brilliance. Dive deeper, post efficiently,
-          and radiate even more.
-        </p>
-      </footer>
+      <Container
+        minH={"300px"}
+        maxW={"container.xl"}
+        paddingBottom={"20px"}
+        borderTop="1px solid black"
+        borderBottom="1px solid black"
+      >
+        <Text
+          fontSize="3xl"
+          color="black"
+          lineHeight="tall"
+          textAlign="center"
+          as="b"
+        >
+          TODO: Diagram about how this shit works
+        </Text>
+      </Container>
+
+      <Container minH={"450px"} maxW={"container.xl"} paddingBottom={"20px"}>
+        <Box textAlign="center" paddingTop="20px" paddingBottom="20px">
+          <Text
+            fontSize="3xl"
+            color="black"
+            lineHeight="tall"
+            textAlign="center"
+            as="b"
+          >
+            🔥 Customers can't stop raving about their growth
+          </Text>
+        </Box>
+
+        {/* TODO: refactor these review cards to be their own component */}
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing="40px">
+          <Card>
+            <CardHeader marginBottom={"1px"} paddingBottom="8px">
+              <Heading size="lg">⭐ ⭐ ⭐ ⭐ ⭐</Heading>
+            </CardHeader>
+            <CardBody marginTop={"1px"} paddingTop="8px">
+              <Text lineHeight="tall" fontSize="md">
+                Photo AI is just fantastic! I take amazing photos of my wife,
+                family and friends. As a photographer I use it to test ideas
+                before creating a real photoshoot. I strongly recommend
+              </Text>
+            </CardBody>
+            <Divider color="gray" />
+            <CardFooter>
+              <Flex width="100%">
+                <Box flex="1">
+                  <Text as="b">Adolf H.</Text>
+                </Box>
+                <Spacer />
+                <Box>
+                  <Badge ml="1" colorScheme="green">
+                    VERIFIED PURCHASE
+                  </Badge>
+                </Box>
+              </Flex>
+            </CardFooter>
+          </Card>
+
+          <Card>
+            <CardHeader marginBottom={"1px"} paddingBottom="8px">
+              <Heading size="lg">⭐ ⭐ ⭐ ⭐ ⭐</Heading>
+            </CardHeader>
+            <CardBody marginTop={"1px"} paddingTop="8px">
+              <Text lineHeight="tall" fontSize="md">
+                Cool AI tool for image generation! I could create a lot of truly
+                amazing pictures in different locations with different outfits!
+                All my friends were surprised and loved my pictures!
+              </Text>
+            </CardBody>
+            <Divider color="gray" />
+            <CardFooter>
+              <Flex width="100%">
+                <Box flex="1">
+                  <Text as="b">Adolf H.</Text>
+                </Box>
+                <Spacer />
+                <Box>
+                  <Badge ml="1" colorScheme="green">
+                    VERIFIED PURCHASE
+                  </Badge>
+                </Box>
+              </Flex>
+            </CardFooter>
+          </Card>
+
+          <Card>
+            <CardHeader marginBottom={"1px"} paddingBottom="8px">
+              <Heading size="lg">⭐ ⭐ ⭐ ⭐ ⭐</Heading>
+            </CardHeader>
+            <CardBody marginTop={"1px"} paddingTop="8px">
+              <Text lineHeight="tall" fontSize="md">
+                Good input = good output. Very fun! Took me some effort to get
+                the models to feel accurate but once I got the right input it
+                was amazing. Photo AI was very responsive to my questions.
+              </Text>
+            </CardBody>
+            <Divider color="gray" />
+            <CardFooter>
+              <Flex width="100%">
+                <Box flex="1">
+                  <Text as="b">Adolf H.</Text>
+                </Box>
+                <Spacer />
+                <Box>
+                  <Badge ml="1" colorScheme="green">
+                    VERIFIED PURCHASE
+                  </Badge>
+                </Box>
+              </Flex>
+            </CardFooter>
+          </Card>
+        </SimpleGrid>
+      </Container>
+
+      <Container
+        minH={"300px"}
+        maxW={"container.xl"}
+        paddingBottom={"20px"}
+        borderTop="1px solid black"
+        borderBottom="1px solid black"
+      >
+        <Text
+          fontSize="3xl"
+          color="black"
+          lineHeight="tall"
+          textAlign="center"
+          as="b"
+        >
+          TODO: Pitch section
+        </Text>
+      </Container>
+
+      <Container
+        minH={"300px"}
+        maxW={"container.xl"}
+        paddingBottom={"20px"}
+        borderTop="1px solid black"
+        borderBottom="1px solid black"
+      >
+        <Text
+          fontSize="3xl"
+          color="black"
+          lineHeight="tall"
+          textAlign="center"
+          as="b"
+        >
+          TODO: Bottom CTA
+        </Text>
+      </Container>
     </div>
   );
 }
