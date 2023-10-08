c31f93acc5fe14bf7c9e9221d3ee54866ba67252
zwickers <jaz2130@columbia.edu>
2023-10-08 16:07:38 -0600

    integrate ChakraUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,9 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    /* TODO: for code cleanliness purposes, ClerkProvider should live inside of providers.tsx,
+    but when we do that, we get a "`NextRouter` was not mounted" error */
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
