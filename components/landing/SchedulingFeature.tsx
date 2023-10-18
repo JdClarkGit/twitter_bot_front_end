6b04274d9d3d2f2dc9cc7bb7db196ba64f2dbd3e
zwickers <jaz2130@columbia.edu>
2023-10-18 15:10:11 -0600

    scheduling feature

--- components/landing/SchedulingFeature.tsx
+++ components/landing/SchedulingFeature.tsx
@@ -20,7 +20,7 @@ import {
     createIcon,
     Link,
   } from "@chakra-ui/react";
-  import { CalendarIcon } from "@chakra-ui/icons";
+  import { CalendarIcon, TimeIcon, LinkIcon } from "@chakra-ui/icons";
 
 const SchedulingFeature = () => {
 return (<Container
@@ -57,7 +57,7 @@ return (<Container
       spacing="40px"
       marginTop="50px"
     >
-      <Card background="none" borderColor="gray.300" borderWidth="0.5px" _hover={{
+      <Card boxShadow="4px 4px 8px rgba(255, 255, 255, 0.5)" background="none" borderColor="gray.300" borderWidth="0.5px" _hover={{
     "&::before": {
       content: '""',
       position: "absolute",
@@ -76,12 +76,12 @@ return (<Container
             Queue your post with a single click - or pick a time manually.
           </Text>
           <Center marginTop="25px">
-          <CalendarIcon boxSize="30%" color="white"/>
+          <CalendarIcon boxSize="20%" color="green.400"/>
           </Center>
         </CardBody>
       </Card>
 
-      <Card background="none" borderColor="gray.300" borderWidth="0.5px" _hover={{
+      <Card boxShadow="4px 4px 8px rgba(255, 255, 255, 0.5)" background="none" borderColor="gray.300" borderWidth="0.5px" _hover={{
     "&::before": {
       content: '""',
       position: "absolute",
@@ -93,19 +93,19 @@ return (<Container
     },
   }}>
         <CardHeader marginBottom={"1px"} paddingBottom="8px">
-          <Heading size="lg" color="white">Schedule with one click.</Heading>
+          <Heading size="lg" color="white">Pick the perfect time.</Heading>
         </CardHeader>
         <CardBody marginTop={"1px"} paddingTop="8px">
           <Text lineHeight="tall" fontSize="md" color="white">
-            Queue your post with a single click - or pick a time manually.
+            Time each post to perfection using our performance analytics.
           </Text>
           <Center marginTop="25px">
-          <CalendarIcon boxSize="30%" color="white"/>
+          <TimeIcon boxSize="20%" color="green.400"/>
           </Center>
         </CardBody>
       </Card>
 
-      <Card background="none" borderColor="gray.300" borderWidth="0.5px" _hover={{
+      <Card boxShadow="4px 4px 8px rgba(255, 255, 255, 0.5)" background="none" borderColor="gray.300" borderWidth="0.5px" _hover={{
     "&::before": {
       content: '""',
       position: "absolute",
@@ -117,14 +117,14 @@ return (<Container
     },
   }}>
         <CardHeader marginBottom={"1px"} paddingBottom="8px">
-          <Heading size="lg" color="white">Schedule with one click.</Heading>
+          <Heading size="lg" color="white">Boost your content.</Heading>
         </CardHeader>
         <CardBody marginTop={"1px"} paddingTop="8px">
           <Text lineHeight="tall" fontSize="md" color="white">
-            Queue your post with a single click - or pick a time manually.
+            Automatically retweet and plug your posts for boosted engagement.
           </Text>
           <Center marginTop="25px">
-          <CalendarIcon boxSize="20%" color="white"/>
+          <LinkIcon boxSize="20%" color="green.400"/>
           </Center>
         </CardBody>
       </Card>
