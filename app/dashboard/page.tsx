4715547ac34bd621717815a7169fc46688588ebb
Joseph D. Clark <josephclark@Josephs-MacBook-Air.local>
2023-10-22 22:35:50 -0500

    edited dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import Link from "next/link";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 library.add(faSearch); // Adds the search icon to the library
+import React from "react";
+import Link from "next/link";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
 
 export default function DashboardPage() {
   const menuItems = [
@@ -16,100 +25,95 @@ export default function DashboardPage() {
   ];
 
   return (
-    <div className="flex h-screen w-screen bg-white">
+    <Flex height="100vh" width="100vw" backgroundColor="white">
       {/* Sidebar Menu */}
-      <div className="flex flex-col space-y-12 p-5 border-gray-300 border-r-2 bg-gray-800">
+      <Box width="250px" paddingY={5}>
         {menuItems.map((item, index) => (
-          <div key={index} className="w-full">
+          <Box key={index} mb={3} textAlign="center">
             {item.href ? (
-              <Link href={item.href}>
-                <div
-                  className="inline-flex items-center
-                  justify-center text-sm font-medium transition-colors
-                  focus-visible:outline-none focus-visible:ring-2
-                  focus-visible:ring-offset-2 disabled:opacity-50
-                  disabled:pointer-events-none ring-offset-background
-                  text-white h-12 py-4 px-8 bg-[#256861] rounded-full
-                  hover:bg-gray-800"
-                >
+              <LinkBox as="div">
+                <LinkOverlay as={Link} href={item.href}>
                   {item.alt}
-                </div>
-              </Link>
+                </LinkOverlay>
+              </LinkBox>
             ) : (
-              <button
-                onClick={item.onClick}
-                className="inline-flex items-center
-                  justify-center text-sm font-medium transition-colors
-                  focus-visible:outline-none focus-visible:ring-2
-                  focus-visible:ring-offset-2 disabled:opacity-50
-                  disabled:pointer-events-none ring-offset-background
-                  text-white h-12 py-4 px-8 bg-blue-600 rounded-full
-                  hover:bg-gray-800"
-              >
-                {item.alt}
-              </button>
+              <Button variant="ghost">{item.alt}</Button>
             )}
-          </div>
+          </Box>
         ))}
-      </div>
+      </Box>
 
       {/* Dashboard Content */}
-      <div className="flex flex-col flex-1 p-5 overflow-y-auto bg-white text-black">
+      <Box flex="1" p={5}>
         {/* Header Area */}
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-semibold">Dashboard</h2>
-          <div className="flex space-x-4 relative">
-            <button>🔔</button>{" "}
-            {/* Notification Icon (replace with a real icon) */}
-            <div className="relative w-64">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="border rounded-lg pl-10 p-2 w-full"
-              />
-              <span className="absolute left-3 top-1/2 transform -translate-y-1/2">
-                <FontAwesomeIcon icon="search" />
-              </span>
-            </div>
-          </div>
-        </div>
+        <Flex justifyContent="space-between" alignItems="center" mb={4}>
+          <Heading as="h2" size="lg">
+            Dashboard
+          </Heading>
+          <Box>
+            <Button variant="ghost" mr={3}>
+              🔔
+            </Button>{" "}
+            {/* Notification Icon */}
+            <Input
+              type="text"
+              placeholder="Search..."
+              variant="filled"
+              rounded="lg"
+              paddingLeft="40px"
+              maxWidth="300px"
+            />
+            {/* FontAwesome icon can be placed here if desired */}
+          </Box>
+        </Flex>
 
         {/* Main Content Area */}
-        <div className="flex flex-wrap gap-4">
-          <Link
-            href="/create"
-            className="p-2 w-1/3 border rounded shadow text-center h-12 hover:cursor-pointer hover:bg-white hover:text-[#256861] bg-[#256861] text-white"
+        <Flex wrap="wrap" gap={6} mt={4}>
+          {/* CRM Link */}
+          <LinkBox
+            as="div"
+            w="1/2" // <-- Adjusted width here
+            p={4}
+            borderWidth="1px"
+            borderRadius="md"
+            boxShadow="md"
+            textAlign="center"
+            _hover={{ bgColor: "white", color: "#256861", cursor: "pointer" }}
+            bg="#256861"
+            color="white"
           >
-            CRM
-          </Link>
-          {/* Recent Notes Card */}
-          <div className="p-2 w-1/3 border rounded shadow text-center h-20 hover:cursor-pointer hover:bg-white hover:text-[#256861] bg-[#256861] text-white">
-            <h3 className="text-xl mb-4">Recent Notes</h3>
-            <Link href="/notes" className="underline">
+            <LinkOverlay as={Link} href="/create" size="md">
+              CRM Dashboard
+            </LinkOverlay>
+          </LinkBox>
+
+          {/* Recent Notes */}
+          <LinkBox
+            as="div"
+            w="1/3"
+            p={4}
+            borderWidth="1px"
+            borderRadius="md"
+            boxShadow="md"
+            textAlign="center"
+            _hover={{ bgColor: "white", color: "#256861", cursor: "pointer" }}
+            bg="#256861"
+            color="white"
+          >
+            <Heading size="md" mb={3}>
+              Recent Notes
+            </Heading>
+            <LinkOverlay as={Link} href="/notes">
               View All
-            </Link>
-            {/* ... list of recent notes ... */}
-          </div>
+            </LinkOverlay>
+          </LinkBox>
 
-          {/* Tasks for Today Card */}
-          <div className="p-2 w-1/3 border rounded shadow text-center h-12 hover:cursor-pointer hover:bg-white hover:text-[#256861] bg-[#256861] text-white">
-            <h3 className="text-xl mb-4">Tasks for Today</h3>
-            {/* ... list of tasks ... */}
-          </div>
+          {/* Other cards can follow in similar fashion */}
+        </Flex>
 
-          {/* Quick Stats Card */}
-          <div className="p-2 w-1/3 border rounded shadow text-center h-12 hover:cursor-pointer hover:bg-white hover:text-[#256861] bg-[#256861] text-white">
-            <h3 className="text-xl mb-4">Quick Stats</h3>
-            {/* ... some stats ... */}
-          </div>
-          <div className="p-2 w-1/3 border rounded shadow text-center h-12 hover:cursor-pointer hover:bg-white hover:text-[#256861] bg-[#256861] text-white">
-            <Link href="/faq">FAQ</Link>
-            {/* ... other links ... */}
-          </div>
-        </div>
-
-        {/* Footer Area */}
-      </div>
-    </div>
+        {/* Footer Area (if needed) */}
+        {/* ... */}
+      </Box>
+    </Flex>
   );
 }
