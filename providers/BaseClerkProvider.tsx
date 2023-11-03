de71369a837acc3ce205d19cc86bf2ff0c0d2879
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-11-02 22:12:00 -0500

    Succes

--- providers/BaseClerkProvider.tsx
+++ providers/BaseClerkProvider.tsx
@@ -2,7 +2,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 import { auth } from "@clerk/nextjs";
 
-function BaseClerkProvider({ children }: { children: React.ReactNode }) {
+function BaseClerkProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}): React.JSX.Element {
   const { userId } = auth();
 
   // Render children even if there's no userId. You can adjust this logic
